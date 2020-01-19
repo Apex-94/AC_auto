444f0183ce4b5278aae325912f806c7c79fe8066
Apex <ahilkaiz@gmail.com>
2020-01-19 20:43:19 +0530

    Updated line tracking

--- AC_auto/main.py
+++ AC_auto/main.py
@@ -19,35 +19,142 @@
 
 import mss
 import time
-
+from statistics import mean
 import cv2
 import mss
 import numpy as np
 import pyautogui
+from numpy import ones,vstack
+from numpy.linalg import lstsq
+# import pyopencl as cl
+
 from directkeys import PressKey, W, A, S, D, ReleaseKey
+# def draw_lines(img, lines):
+#     try:
+#         for line in lines:
+#             coords = line[0]
+#             cv2.line(img, (coords[0],coords[1]), (coords[2],coords[3]), [255,255,255], 3)
+#     except:
+#         pass
 
-def draw_lines(img, lines):
+def draw_lanes(img, lines, color=[0, 255, 255], thickness=3):
+    # if this fails, go with some default line
     try:
-        for line in lines:
-            coords = line[0]
-            cv2.line(img, (coords[0],coords[1]), (coords[2],coords[3]), [255,255,255], 3)
-    except:
-        pass
 
+        # finds the maximum y value for a lane marker
+        # (since we cannot assume the horizon will always be at the same point.)
+
+        ys = []
+        for i in lines:
+            for ii in i:
+                ys += [ii[1], ii[3]]
+        min_y = min(ys)
+        max_y = 600
+        new_lines = []
+        line_dict = {}
+
+        for idx, i in enumerate(lines):
+            for xyxy in i:
+                # These four lines:
+                # modified from http://stackoverflow.com/questions/21565994/method-to-return-the-equation-of-a-straight-line-given-two-points
+                # Used to calculate the definition of a line, given two sets of coords.
+                x_coords = (xyxy[0], xyxy[2])
+                y_coords = (xyxy[1], xyxy[3])
+                A = vstack([x_coords, ones(len(x_coords))]).T
+                m, b = lstsq(A, y_coords)[0]
+
+                # Calculating our new, and improved, xs
+                x1 = (min_y - b) / m
+                x2 = (max_y - b) / m
+
+                line_dict[idx] = [m, b, [int(x1), min_y, int(x2), max_y]]
+                new_lines.append([int(x1), min_y, int(x2), max_y])
+
+        final_lanes = {}
+
+        for idx in line_dict:
+            final_lanes_copy = final_lanes.copy()
+            m = line_dict[idx][0]
+            b = line_dict[idx][1]
+            line = line_dict[idx][2]
+
+            if len(final_lanes) == 0:
+                final_lanes[m] = [[m, b, line]]
+
+            else:
+                found_copy = False
+
+                for other_ms in final_lanes_copy:
+
+                    if not found_copy:
+                        if abs(other_ms * 1.2) > abs(m) > abs(other_ms * 0.8):
+                            if abs(final_lanes_copy[other_ms][0][1] * 1.2) > abs(b) > abs(
+                                    final_lanes_copy[other_ms][0][1] * 0.8):
+                                final_lanes[other_ms].append([m, b, line])
+                                found_copy = True
+                                break
+                        else:
+                            final_lanes[m] = [[m, b, line]]
+
+        line_counter = {}
+
+        for lanes in final_lanes:
+            line_counter[lanes] = len(final_lanes[lanes])
+
+        top_lanes = sorted(line_counter.items(), key=lambda item: item[1])[::-1][:2]
+
+        lane1_id = top_lanes[0][0]
+        lane2_id = top_lanes[1][0]
+
+        def average_lane(lane_data):
+            x1s = []
+            y1s = []
+            x2s = []
+            y2s = []
+            for data in lane_data:
+                x1s.append(data[2][0])
+                y1s.append(data[2][1])
+                x2s.append(data[2][2])
+                y2s.append(data[2][3])
+            return int(mean(x1s)), int(mean(y1s)), int(mean(x2s)), int(mean(y2s))
+
+        l1_x1, l1_y1, l1_x2, l1_y2 = average_lane(final_lanes[lane1_id])
+        l2_x1, l2_y1, l2_x2, l2_y2 = average_lane(final_lanes[lane2_id])
+
+        return [l1_x1, l1_y1, l1_x2, l1_y2], [l2_x1, l2_y1, l2_x2, l2_y2]
+    except Exception as e:
+        print(str(e))
 
 def process_img(og_img):
-    processed_img = cv2.cvtColor(og_img, cv2.COLOR_BGR2GRAY)
-    processed_img = cv2.Canny(processed_img, threshold1=120, threshold2=220)
-    processed_img = cv2.GaussianBlur(processed_img, (5,5), 0)
-    vertices = np.array([[10,500], [10,300], [300,200], [500,200] ,[800,300], [800,500]])
+    # processed_img = cv2.cvtColor(og_img, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(og_img, threshold1=100, threshold2=200)
+    processed_img = cv2.GaussianBlur(processed_img, (3,3), 0)
+    vertices = np.array([[10,500], [10,300], [300,200], [500,200] ,[800,300], [800,500]], np.int32)
     processed_img = roi(processed_img,[vertices])
-    lines = cv2.HoughLinesP(processed_img, 1, np.pi/120, 180, 20, 15) #edges
-    draw_lines(processed_img, lines)
-    return processed_img
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 200, 5) #edges
+    try:
+        l1, l2 = draw_lanes(og_img, lines)
+        cv2.line(og_img, (l1[0], l1[1]), (l1[2], l1[3]), [0, 255, 0], 30)
+        cv2.line(og_img, (l2[0], l2[1]), (l2[2], l2[3]), [0, 255, 0], 30)
+    except Exception as e:
+        print(str(e))
+        pass
+    try:
+        for coords in lines:
+            coords = coords[0]
+            try:
+                cv2.line(processed_img, (coords[0], coords[1]), (coords[2], coords[3]), [255, 0, 0], 3)
+
+
+            except Exception as e:
+                print(str(e))
+    except Exception as e:
+        pass
+    return processed_img,og_img
 
 # for i in list(range(5))[::-1]:
 #     print(i+1)
-#     time.sleep(1)
+#     time.sleep(1)s
 #
 #
 # PressKey(W)
@@ -70,16 +177,23 @@ def roi(img, vertices):
 with mss.mss() as sct:
     # Part of the screen to capture
     monitor = {"top": 40, "left": 0, "width": 800, "height": 640}
-
+    ct = 0
     while "Screen capturing":
         last_time = time.time()
-
+        # ct = 0
         # Get raw pixels from the screen, save it to a Numpy array
         screen = np.array(sct.grab(monitor))
 
-        new_screen = process_img(screen)
+        # if ct % 3 == 0:
+        #     screen = np.array(sct.grab(monitor))
+        #
+        # ct=ct+1
+        new_screen,og_img = process_img(screen)
         # Display the picture
+
+
         cv2.imshow("window", new_screen)
+        cv2.imshow('window2', cv2.cvtColor(og_img, cv2.COLOR_BGR2RGB))
 
         # Display the picture in grayscale
         # cv2.imshow('OpenCV/Numpy grayscale',
